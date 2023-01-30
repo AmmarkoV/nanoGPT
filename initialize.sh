d353a39c3dd829fdd7a290030c80077467f656ab
Ammar Qammaz <ammarkov@gmail.com>
2023-01-30 12:50:06 +0200

    ...

--- initialize.sh
+++ initialize.sh
@@ -2,12 +2,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-
-
 python3 -m venv venv
 source venv/bin/activate
-python3 -m pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu116
 python3 -m pip install --upgrade pip 
+python3 -m pip install torch torchvision numpy --extra-index-url https://download.pytorch.org/whl/cu116
 python3 -m pip install transformers datasets tiktoken wandb tqdm
 
 
